2af6ab7e8281357d3aead904d8ebc28fead315b8
gopalmenon <gopal.mnn@gmail.com>
2015-05-17 13:10:45 -0600

    Finished with recursive method

--- CS 5050 Assignment 1/src/menon/cs5050/assignment1/TwoKnapsacks.java
+++ CS 5050 Assignment 1/src/menon/cs5050/assignment1/TwoKnapsacks.java
@@ -13,10 +13,35 @@ public class TwoKnapsacks {
 		this.knapsack2Size = knapsack2Size;
 	}
 	
-	boolean knap() {
-		return true;
+	//Call a recursive method that checks if there exists a subset of objects that fill both knapsacks exactly
+	public boolean knap() {	
+		
+		return willTheyExactlyFitRecursive(this.objectSizes.length, this.knapsack1Size, this.knapsack2Size);
 	}
 	
-	
+	//This is the recursive method called from method knap
+	private boolean willTheyExactlyFitRecursive(int numberOfObjects, int knapsack1Size, int knapsack2Size) {
+		
+		//Both knapsacks are empty, then it means that they are filled
+		if (knapsack1Size == 0 && knapsack2Size == 0) {
+			return true;
+		}
+		
+		//If there are no more objects left, since the knapsacks are both not empty, it means that the objects do not
+		//fill the knapsacks exactly
+		if (numberOfObjects == 0) {
+			return false;
+		}
+		
+		//The knapsacks should not have a negative length
+		if (knapsack1Size < 0 || knapsack2Size < 0) {
+			return false;
+		}
+		
+		return willTheyExactlyFitRecursive(numberOfObjects - 1, knapsack1Size, knapsack2Size) || //discard the object
+			   willTheyExactlyFitRecursive(numberOfObjects - 1, knapsack1Size - this.objectSizes[numberOfObjects], knapsack2Size) || //put the object in knapsack1
+		       willTheyExactlyFitRecursive(numberOfObjects - 1, knapsack1Size, knapsack2Size - this.objectSizes[numberOfObjects]); //put the object in knapsack2
+
+	}
 
 }
