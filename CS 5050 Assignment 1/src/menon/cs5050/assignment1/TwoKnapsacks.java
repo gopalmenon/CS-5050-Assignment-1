2652cf553c817955ddd4b5ea255d5ab56443465e
gopalmenon <gopal.mnn@gmail.com>
2015-05-17 11:25:25 -0600

    Initial commit

---
+++ CS 5050 Assignment 1/src/menon/cs5050/assignment1/TwoKnapsacks.java
@@ -0,0 +1,22 @@
+package menon.cs5050.assignment1;
+
+public class TwoKnapsacks {
+	
+	int objectSizes[]; //array containing object sizes 
+	int knapsack1Size; //Size of first knapsack
+	int knapsack2Size; //Size of second knapsack
+	
+	//Constructor
+	public TwoKnapsacks(int objectSizes[], int knapsack1Size, int knapsack2Size) {
+		this.objectSizes = objectSizes;
+		this.knapsack1Size = knapsack1Size;
+		this.knapsack2Size = knapsack2Size;
+	}
+	
+	boolean knap() {
+		return true;
+	}
+	
+	
+
+}
